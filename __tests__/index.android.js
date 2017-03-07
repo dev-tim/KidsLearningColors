f4eac0d2875fb9ef68c731cdf3f92f74ed8d794d
dev-tim <artem.zadorozhniy@gmail.com>
2017-03-07 23:13:50 +0100

    Initi commit

---
+++ __tests__/index.android.js
@@ -0,0 +1,24 @@
+import 'react-native';
+import React from 'react';
+import Index from '../index.android.js';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <Index />
+  );
+});
+
+
+export function loadItems(id) {
+  return {type: 'LOAD_ITEMS', data: {id}}
+}
+
+
+
+
+
+
+
